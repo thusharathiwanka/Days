cca4ee29be1e200a0f5dcf7442047f4665a5aadd
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-11-01 01:38:13 +0530

    CREATE: animation added to landing

---
+++ script.js
@@ -0,0 +1,26 @@
+let controller;
+let slideScene;
+
+function animateSlides() {
+  controller = new ScrollMagic.Controller();
+
+  const slides = document.querySelectorAll(".slide");
+  const nav = document.querySelector(".nav-header");
+
+  slides.forEach((slide) => {
+    const revealImg = slide.querySelector(".reveal-img");
+    const img = slide.querySelector("img");
+    const revealText = slide.querySelector(".reveal-text");
+
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
+
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+    slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
+    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.75");
+  });
+}
+
+animateSlides();
