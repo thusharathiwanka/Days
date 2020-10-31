2d0d04e8f28939384e136de970ef1db4fc47992e
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-11-01 02:14:32 +0530

    UPDATE: animation flickering fixes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //defining some variables
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
   //initializing controller
@@ -11,11 +12,10 @@ function animateSlides() {
   const nav = document.querySelector(".nav-header");
 
   //looping through each slide class
-  slides.forEach((slide) => {
+  slides.forEach((slide, index, slides) => {
     const revealImg = slide.querySelector(".reveal-img");
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
-
     //adding gsap - creating timeline
     const slideTl = gsap.timeline({
       defaults: { duration: 1, ease: "power2.inOut" },
@@ -37,6 +37,28 @@ function animateSlides() {
         colorTrigger: "white",
         name: "slide",
       })
+      .addTo(controller);
+
+    //adding new timeline
+    const pageTl = gsap.timeline();
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    pageTl.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+    //create new scene
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "page",
+        indent: 200,
+      })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageTl)
       .addTo(controller);
   });
 }
